9368036a317aa7cdc802f199987adbcc8b48f007
baba_s <shotababa1206@gmail.com>
2020-04-19 09:59:44 +0900

    first commit

---
+++ Editor/BooLangKiller.cs
@@ -0,0 +1,39 @@
+﻿using SyntaxTree.VisualStudio.Unity.Bridge;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using UnityEditor;
+
+namespace UniBooLangKiller
+{
+	[InitializeOnLoad]
+	internal static class BooLangKiller
+	{
+		static BooLangKiller()
+		{
+			ProjectFilesGenerator.ProjectFileGeneration += OnGeneration;
+		}
+
+		private static string OnGeneration( string name, string content )
+		{
+			var document = XDocument.Parse( content );
+
+			document.Root?.Descendants()
+				.Where( c => c.Name.LocalName == "Reference" )
+				.Where( c => c.Attribute( "Include" )?.Value == "Boo.Lang" )
+				.Remove()
+				;
+
+			var stream = new Utf8StringWriter();
+			document.Save( stream );
+
+			return stream.ToString();
+		}
+
+		private sealed class Utf8StringWriter : StringWriter
+		{
+			public override Encoding Encoding => Encoding.UTF8;
+		}
+	}
+}
